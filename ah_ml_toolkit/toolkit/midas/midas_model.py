7ae4d8103e2bb95f0fdfb42a7d60bb789cfb81f3
Alexander Hill <me@alxhill.com>
2023-01-08 13:10:19 -0500

    moving stuff around, etc

--- ah_ml_toolkit/toolkit/midas/midas_model.py
+++ ah_ml_toolkit/toolkit/midas/midas_model.py
@@ -1,7 +1,10 @@
 import torch
 
+from ah_ml_toolkit.toolkit.midas.external.dpt_depth import DPTDepthModel
+
 
 class MidasModel(torch.nn.Module):
-
-    def load(self, path: str):
-        pass
+    def __init__(self, model_weights_path: str, size: tuple[int, int]):
+        super().__init__()
+        self.size = size
+        self.depth_model = DPTDepthModel(model_weights_path, True, )
